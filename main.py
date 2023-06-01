c7d4e9a96223d0bbd554360a1934aa0053a4ad97
Czxxxxxxxxx <1015408218@qq.com>
2023-06-01 19:03:45 +0800

    complete readfile
    complete readfile

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import math
 script_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_directory)
 
-dt = np.dtype(int)
-
 class Individual:
     def __init__(self, chromosome_length):
         self.chromosome = [random.randint(0, 1) for _ in range(chromosome_length)]
-        self.fitness = 0
+        self.chromosome_length = chromosome_length
+        self.fitness =
 
-    def calculate_fitness(self, fitness_function):
+    def random_permutation(self):
+        nums = list(range(1, self.chromosome_length + 1))
+        for i in range(self.chromosome_length - 1):
+            j = random.randint(i, self.chromosome_length - 1)
+            nums[i], nums[j] = nums[j], nums[i]
+
+        self.chromosome = nums
+
+    def calculate_fitness(self,fitness_function):
         self.fitness = fitness_function(self.chromosome)
 
 
@@ -33,8 +40,42 @@ class EvolutionaryAlgorithm:
             n = int(file.readline().strip())  # 读取数字n
             self.chromosome_length = n
             print(n)
+            # 读取矩阵A
+            rows_A = []
+            i = 0
+            while i < n :
+                row = list(map(int, file.readline().strip().split()))
+                if len(row) == 0:
+                    continue
+                row = np.array(row)
+                rows_A.append(row)
+                i += 1
+            A = np.array(rows_A,dtype=object)
+
+            # 读取矩阵B
+            rows_B = []
+            i = 0
+            while i < n:
+                row = list(map(int, file.readline().strip().split()))
+                if len(row) == 0:
+                    continue
+                row = np.array(row)
+                rows_B.append(row)
+                i += 1
+            B = np.array(rows_B,dtype=object)
+        self.Matrix_D = A
+        self.Matrix_F = B
+        # # 打印矩阵A和B
+        # print("Matrix A:")
+        # print(A)
+        # print("\nMatrix B:")
+        # print(B)
+
+def getcost(A,B,perm):
+
 
 
 if __name__ == '__main__':
     datafile = "./qapdata/chr12a.dat"
-    EvolutionaryAlgorithm(1,2,3,4,datafile)
+
+    EvolutionaryAlgorithm(population_size=100,fitness_function=2,mutation_rate=0.1,crossover_rate=0.1,data_filename=datafile)
