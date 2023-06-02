98c2cc3e827be2d17ea7f91d498c577b009d6f10
Czxxxxxxxxx <1015408218@qq.com>
2023-06-02 11:23:21 +0800

    Mutation
    complete mutation

--- main.py
+++ main.py
@@ -38,6 +38,10 @@ class EvolutionaryAlgorithm:
         self.data_filename = data_filename
         self.ReadFile()
 
+        self.population.append(Individual(5,[1,2,3,4,5]))
+        self.insert_mutation()
+        print(self.population[0].chromosome)
+
     def ReadFile(self):
         # 从文件中读取数据
         with open(self.data_filename, 'r') as file:
@@ -80,6 +84,32 @@ class EvolutionaryAlgorithm:
         for individual in self.population:
             individual.calculate_fitness(self.fitness_function,self.Matrix_D,self.Matrix_F)
 
+    def scramble_mutation(self): #争夺变异，中间随机排序
+        for individual in self.population:
+            if random.random() < self.mutation_rate: #处理变异率
+                # 随机选择一段子串
+                a = random.randint(0, individual.chromosome_length-1)
+                b = random.randint(0, individual.chromosome_length) #结束点可以往后挪，因为[start:end]是左闭右开的
+                start = min(a,b)
+                end = max(a,b)
+                segment = individual.chromosome[start:end]
+                # 随机重新排序子串
+                random.shuffle(segment)
+                # 更新个体的染色体
+                individual.chromosome[start:end] = segment
+
+
+    def insert_mutation(self): #插入变异，保留大部分的邻接关系，但破坏序关系
+        for individual in self.population:
+            if random.random() < self.mutation_rate: #处理变异率
+                # 随机选取两个基因的索引
+                index1 = random.randint(0, individual.chromosome_length - 1)
+                index2 = random.randint(0, individual.chromosome_length - 1)
+                #插入
+                print(index1,index2)
+                gene = individual.chromosome.pop(index2)
+                individual.chromosome.insert(index1 + 1, gene)
+
 
 #计算总运输成本
 def getcost(D,F,perm):
@@ -97,4 +127,4 @@ def getcost(D,F,perm):
 
 if __name__ == '__main__':
     datafile = "./qapdata/nug12.dat"
-    EvolutionaryAlgorithm(population_size=100,fitness_function=getcost,mutation_rate=0.1,crossover_rate=0.1,data_filename=datafile)
+    EvolutionaryAlgorithm(population_size=100,fitness_function=getcost,mutation_rate=1,crossover_rate=0.1,data_filename=datafile)
