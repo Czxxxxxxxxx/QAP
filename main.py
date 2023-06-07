5df2d45f2949ba456be0c213d657e10659b50e61
Czxxxxxxxxx <1015408218@qq.com>
2023-06-07 23:42:17 +0800

    complete framework
    complete framework

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import os
+import copy
 import math
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
@@ -9,16 +10,15 @@ os.chdir(script_directory)
 
 class Individual:
 
-    def __init__(self, chromosome_length):
-        self.chromosome_length = chromosome_length
-        self.chromosome = self.get_random_permutation()
-        self.fitness = -1
-
-    #直接使用列表进行初始化
-    def __init__(self, chromosome_length, list):
-        self.chromosome_length = chromosome_length
-        self.chromosome = list
-        self.fitness = -1
+    def __init__(self, chromosome_length, list=None):
+        if list == None:
+            self.chromosome_length = chromosome_length
+            self.chromosome = self.get_random_permutation()
+            self.fitness = -1
+        else:
+            self.chromosome_length = chromosome_length
+            self.chromosome = list
+            self.fitness = -1
 
     #获取随机的一个解
     def get_random_permutation(self):
@@ -34,30 +34,94 @@ class Individual:
 
 class EvolutionaryAlgorithm:
 
-    def __init__(self, population_size, fitness_function, mutation_rate,
-                 crossover_rate, data_filename):
+    def __init__(self, population_size,  fitness_function,
+                 mutation_rate, crossover_rate, data_filename):
         self.population_size = population_size
         self.fitness_function = fitness_function
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
         self.population = []
+        self.newPop = []
+        self.best_individual = None
         self.data_filename = data_filename
         self.ReadFile()
-
-        self.population.append(Individual(9, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
-        self.population.append(Individual(9, [9, 3, 7, 8, 2, 6, 5, 1, 4]))
+        print(f'self.chromosome_length={self.chromosome_length}')
+        # self.population.append(Individual(9, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+        # self.population.append(Individual(9, [9, 3, 7, 8, 2, 6, 5, 1, 4]))
         # self.insert_mutation()
         # self.order_crossover()
-        self.cycle_crossover()
-        print(self.population[0].chromosome)
-        print(self.population[1].chromosome)
+        # self.cycle_crossover()
+        # print(self.population[0].chromosome)
+        # print(self.population[1].chromosome)
+
+    def run(self, num_generations):
+        self.initialize_population()
+        for i in range(1, num_generations + 1):
+            self.evaluate_population()
+            self.getBest()
+            self.newPop = [self.getBest()]  #精英保留
+            self.roulette_wheel_selection() #父代选择
+            # self.order_crossover()          #交叉之后，newpop的数量为原数量+1（保留了1个精英）
+            self.cycle_crossover()
+            # self.insert_mutation()
+            self.scramble_mutation()
+            self.evaluate_newPop()
+            self.Substitude()
+
+            print(f'generation={i},bestchrome={self.population[0].chromosome},fitness={self.population[0].fitness}')
+        bestin = self.getBest()
+        print(f'bestchrome={bestin.chromosome},fitness={bestin.fitness}')
+
+
+    def roulette_wheel_selection(self):
+        total_fitness = sum(individual.fitness for individual in self.population)
+        probabilities = [individual.fitness / total_fitness for individual in self.population]
+        cumulative_probabilities = [sum(probabilities[:i+1]) for i in range(self.population_size)]
+        # print(f'probabilities={probabilities},cumulative_probabilities={cumulative_probabilities}')
+        selected_population = []
+        for _ in range(self.population_size):
+            random_number = random.random()
+            for i in range(self.population_size):
+                if random_number <= cumulative_probabilities[i]:
+                    selected_population.append(self.population[i]) 
+                    break
+
+        self.population = selected_population
+
+    def Substitude(self):   #去掉newPop中适应度最差的解
+        self.newPop.sort(key=lambda individual: individual.fitness)
+        self.population = self.newPop[:self.population_size]
+
+    def printPop(self):
+        for i in range(0, self.population_size):
+            print(f'i={i},chromosome={self.population[i].chromosome},fitness={self.population[i].fitness}')
+
+    def printNewPop(self):
+        for i in range(0, len(self.newPop)):
+            print(f'newpop: i={i},chromosome={self.newPop[i].chromosome},fitness={self.newPop[i].fitness}')
+
+    def initialize_population(self):
+        self.population = [
+            Individual(chromosome_length=self.chromosome_length)
+            for _ in range(self.population_size)
+        ]
+
+    def getBest(self):
+        #获取种群中适应度值最低的个体
+        self.evaluate_population()
+        self.best_individual = min(self.population,
+                                   key=lambda individual: individual.fitness)
+        return self.best_individual
+
+    def AddIndidual(self, perm):
+        self.population.append(Individual(len(perm), perm))
+        self.population_size += 1
 
     def ReadFile(self):
         # 从文件中读取数据
         with open(self.data_filename, 'r') as file:
             n = int(file.readline().strip())  # 读取数字n
             self.chromosome_length = n
-            # print(n)
             # 读取矩阵A
             rows_A = []
             i = 0
@@ -84,7 +148,7 @@ class EvolutionaryAlgorithm:
 
         self.Matrix_F = A
         self.Matrix_D = B
-        # # 打印矩阵A和B
+        # 打印矩阵A和B
         # print("Matrix A:")
         # print(A)
         # print("\nMatrix B:")
@@ -94,9 +158,17 @@ class EvolutionaryAlgorithm:
         for individual in self.population:
             individual.calculate_fitness(self.fitness_function, self.Matrix_D,
                                          self.Matrix_F)
+            # print(individual.fitness)
+    def evaluate_newPop(self):
+        for individual in self.newPop:
+            individual.calculate_fitness(self.fitness_function, self.Matrix_D,
+                                         self.Matrix_F)
+            # print(individual.fitness)        
 
     def scramble_mutation(self):  #争夺变异，中间随机排序
-        for individual in self.population:
+        for individual in self.newPop:
+            if individual == self.best_individual: #精英保留
+                continue
             if random.random() < self.mutation_rate:  #处理变异率
                 # 随机选择一段子串
                 a = random.randint(0, individual.chromosome_length - 1)
@@ -111,7 +183,9 @@ class EvolutionaryAlgorithm:
                 individual.chromosome[start:end] = segment
 
     def insert_mutation(self):  #插入变异，保留大部分的邻接关系，但破坏序关系
-        for individual in self.population:
+        for individual in self.newPop:
+            if individual == self.best_individual: #精英保留
+                continue
             if random.random() < self.mutation_rate:  #处理变异率
                 # 随机选取两个基因的索引
                 index1 = random.randint(0, individual.chromosome_length - 1)
@@ -140,8 +214,8 @@ class EvolutionaryAlgorithm:
                 offspring1[start:end + 1] = parent1.chromosome[start:end + 1]
                 offspring2[start:end + 1] = parent2.chromosome[start:end + 1]
                 # 从另一个父代的第二个交叉点开始填充子代
-                pointer_parent = end + 1
-                pointer_offspring = end + 1
+                pointer_parent = (end + 1) % parent1.chromosome_length
+                pointer_offspring = (end + 1) % parent1.chromosome_length
                 while None in offspring1:
                     if parent2.chromosome[pointer_parent] not in offspring1:
                         offspring1[pointer_offspring] = parent2.chromosome[
@@ -152,8 +226,8 @@ class EvolutionaryAlgorithm:
                                       1) % parent1.chromosome_length
 
                 # 填充第二个子代
-                pointer_parent = end + 1
-                pointer_offspring = end + 1
+                pointer_parent = (end + 1) % parent1.chromosome_length
+                pointer_offspring = (end + 1) % parent1.chromosome_length
                 while None in offspring2:
                     if parent1.chromosome[pointer_parent] not in offspring2:
                         offspring2[pointer_offspring] = parent1.chromosome[
@@ -163,11 +237,11 @@ class EvolutionaryAlgorithm:
                     pointer_parent = (pointer_parent +
                                       1) % parent1.chromosome_length
 
-                # 创建子代个体并替换父代个体
-                self.population[i] = Individual(parent1.chromosome_length,
-                                                offspring1)
-                self.population[i + 1] = Individual(parent1.chromosome_length,
-                                                    offspring2)
+                # 更新子代
+                self.newPop.append(Individual(parent1.chromosome_length,
+                                                offspring1))
+                self.newPop.append(Individual(parent1.chromosome_length,
+                                                    offspring2))
 
     def cycle_crossover(self):  #圈交叉
         for i in range(0, self.population_size - 1, 2):
@@ -188,9 +262,10 @@ class EvolutionaryAlgorithm:
                 # CX算法核心部分
                 while None in circle_flag1:
                     circle_flag2[cur] = circle_idx
-                    cur = parent1.chromosome.index(parent2.chromosome[cur]) #在p1 找 p2的value
+                    cur = parent1.chromosome.index(
+                        parent2.chromosome[cur])  #在p1 找 p2的value
                     circle_flag1[cur] = circle_idx
-                    if(cur == start):
+                    if (cur == start):
                         circle_idx += 1
                         start = circle_flag1.index(None)
                         cur = start
@@ -198,22 +273,24 @@ class EvolutionaryAlgorithm:
                 circle_flag2[cur] = circle_idx
                 # print(f'circle_flag1 = {circle_flag1}, circle_flag2 = {circle_flag2}')
                 #交替选择圈，构造子代
-                flag = 1 #选择第一个父代的元素
+                flag = 1  #选择第一个父代的元素
 
-                for z in range(1,circle_idx+1): #i是圈号，表示当前要复制第i个圈的解
-                    for j in range(0,n):        #j表示当前元素下标
-                        if circle_flag1[j] == z: 
+                for z in range(1, circle_idx + 1):  #i是圈号，表示当前要复制第i个圈的解
+                    for j in range(0, n):  #j表示当前元素下标
+                        if circle_flag1[j] == z:
                             if flag == 1:
                                 offspring1[j] = parent1.chromosome[j]
                                 offspring2[j] = parent2.chromosome[j]
                             else:
                                 offspring1[j] = parent2.chromosome[j]
-                                offspring2[j] = parent1.chromosome[j]    
+                                offspring2[j] = parent1.chromosome[j]
                     flag = not flag
 
-                # 创建子代个体并替换父代个体
-                self.population[i] = Individual(parent1.chromosome_length,offspring1)
-                self.population[i + 1] = Individual(parent1.chromosome_length,offspring2)
+                # 更新子代
+                self.newPop.append(Individual(parent1.chromosome_length,
+                                                offspring1))
+                self.newPop.append(Individual(parent1.chromosome_length,
+                                                    offspring2))
 
                 # print(f'child1 = {offspring1}, child2 = {offspring2}')
 
@@ -234,9 +311,10 @@ def getcost(D, F, perm):
 
 
 if __name__ == '__main__':
-    datafile = "./qapdata/nug12.dat"
-    EvolutionaryAlgorithm(population_size=2,
-                          fitness_function=getcost,
-                          mutation_rate=1,
-                          crossover_rate=1,
-                          data_filename=datafile)
+    datafile = "./qapdata/tai256c.dat"
+    algorithm = EvolutionaryAlgorithm(population_size=50,
+                                      fitness_function=getcost,
+                                      mutation_rate=0.9,
+                                      crossover_rate=0.9,
+                                      data_filename=datafile)
+    algorithm.run(num_generations=10)
